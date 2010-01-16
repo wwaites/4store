73a1186f7ee2f8a1e93f365f9d0161e5cb21b9a3
William Waites <ww@ngwenya.styx.org>
2010-01-16 19:23:38 +0100

    move sync into lock operation, remove possibility of double locks

--- src/backend/lockable.c
+++ src/backend/lockable.c
@@ -2,21 +2,84 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
 #include "lockable.h"
 #include "common/error.h"
 
+/*
+ * Darwin doesn't have a real implementation of fsync(2). Instead, recent
+ * versions provide a fcntl(2) call to actually sync data to disc. Since
+ * we want to know that write operations have happened, we need to use that.
+ * On other systems we just use fsync as normal.
+ * */
+#ifdef _DARWIN_C_SOURCE
+#ifndef F_FULLFSYNC
+#error "No F_FULLFSYNC to reliably get data to disk. Upgrade Darwin to 10.3 or greater."
+#endif
+#define fs_fsync(fd) fcntl(fd, F_FULLFSYNC, NULL)
+#else /* not darwin */
+#define fs_fsync(fd) fsync(fd)
+#endif
+
 extern int errno;
+
+/* flush any cached data to disc after writing out the required metadata */
+static int fs_lockable_sync(fs_lockable_t *hf)
+{
+    /* make sure we have a write lock */
+    assert(hf->locktype & LOCK_EX);
+
+    /* write out any necessary metadata */
+    if ((hf->write_metadata)(hf))
+        return -1;
+    /* flush data to disc */
+    if (fs_fsync(hf->fd)) {
+        fs_error(LOG_ERR, "flock(%s): %s", hf->filename, strerror(errno));
+        return -1;
+    }
+    return 0;
+}
 
 int fs_lockable_lock(fs_lockable_t *hf, int operation)
 {
     struct stat stat;
 
+    /* It is an error to try to upgrade / downgrade locks */
+    if ( (operation & LOCK_EX && hf->locktype & LOCK_SH) ||
+         (operation & LOCK_SH && hf->locktype & LOCK_EX) ) {
+        fs_error(LOG_ERR, "fs_lockable_lock(%s): up/downgrading lock not permitted",
+                 hf->filename);
+        return -1;
+    }
+
+    /* It is an error to request a lock while holding one already */
+    if ( (operation & hf->locktype) & (LOCK_SH|LOCK_EX) ) {
+        fs_error(LOG_ERR, "fs_lockable_lock(%s): double lock", hf->filename);
+        return -1;
+    }
+
+    /* if we are unlocking while holding a write lock, flush data */
+    if ( (hf->locktype & LOCK_EX) && (operation & LOCK_UN) ) {
+        if (fs_lockable_sync(hf))
+            return -1;
+        /* update the mtime before releasing the lock */
+        if (fstat(hf->fd, &stat) < 0) {
+            fs_error(LOG_ERR, "fstat(%s): %s", hf->filename, strerror(errno));
+            return -1;
+        }
+        memcpy(&hf->mtime, &stat.st_mtimespec, sizeof(struct timespec));
+    }
+
+    /* release or acquire the lock */
     if (flock(hf->fd, operation)) {
         fs_error(LOG_ERR, "flock(%s): %s", hf->filename, strerror(errno));
         return -1;
     }
 
-    if ( (operation & LOCK_EX) || (operation & LOCK_SH) ) {
+    hf->locktype = operation;
+
+    /* if we are acquiring the lock, read any metadata if necessary */
+    if ( (operation & (LOCK_EX|LOCK_SH)) ) {
         if (fstat(hf->fd, &stat) < 0) {
             fs_error(LOG_ERR, "fstat(%s): %s", hf->filename, strerror(errno));
             return -1;
@@ -26,9 +89,9 @@ int fs_lockable_lock(fs_lockable_t *hf, int operation)
                (stat.st_mtimespec.tv_nsec > hf->mtime.tv_nsec) ) ) {
            if ( (hf->read_metadata)(hf) )
                return -1;
-           memcpy(&hf->mtime, &stat.st_mtimespec, sizeof(struct timespec));
         }
     }
+
     return 0;
 }
 
@@ -127,27 +190,9 @@ int fs_lockable_init(fs_lockable_t *hf)
         return -1;
     }
 
+    /* set our flag to keep track of the lock type */
+    hf->locktype = LOCK_UN;
+
     return 0;
 }
 
-int fs_lockable_sync(fs_lockable_t *hf)
-{
-    struct stat stat;
-    /* write out any necessary metadata */
-    if ((hf->write_metadata)(hf))
-        return -1;
-    /* flush data to disc */
-    if (fs_fsync(hf->fd)) {
-        fs_error(LOG_ERR, "flock(%s): %s", hf->filename, strerror(errno));
-        return -1;
-    }
-    /* update the mtime */
-    if (fstat(hf->fd, &stat)) {
-        fs_error(LOG_ERR, "fstat(%s): %s", hf->filename, strerror(errno));
-        if (flock(hf->fd, LOCK_UN))
-            fs_error(LOG_ERR, "flock(%s): %s", hf->filename, strerror(errno));
-        return -1;
-    }
-    memcpy(&hf->mtime, &stat.st_mtimespec, sizeof(struct timespec));
-    return 0;
-}
--- src/backend/lockable.h
+++ src/backend/lockable.h
@@ -4,6 +4,7 @@
 typedef struct _fs_lockable_t {
     int fd;
     int flags;
+    int locktype;
     char *filename;
     struct timespec mtime;
     int (*read_metadata)(struct _fs_lockable_t *hf);
@@ -12,21 +13,8 @@ typedef struct _fs_lockable_t {
 
 int fs_lockable_init(fs_lockable_t *);
 int fs_lockable_lock(fs_lockable_t *, int operation);
-int fs_lockable_sync(fs_lockable_t *);
 
-/*
- * Darwin doesn't have a real implementation of fsync(2). Instead, recent
- * versions provide a fcntl(2) call to actually sync data to disc. Since
- * we want to know that write operations have happened, we need to use that.
- * On other systems we just use fsync as normal.
- * */
-#ifdef _DARWIN_C_SOURCE
-#ifndef F_FULLFSYNC
-#error "No F_FULLFSYNC to reliably get data to disk. Upgrade Darwin to 10.3 or greater."
-#endif
-#define fs_fsync(fd) fcntl(fd, F_FULLFSYNC, NULL)
-#else /* not darwin */
-#define fs_fsync(fd) fsync(fd)
-#endif
+/* returns 1 if the type of lock specified by op is held, otherwise 0 */
+#define fs_lockable_test(hf, op) ((hf->locktype & op) ? 1 : 0)
 
 #endif /* LOCKABLE_H */
